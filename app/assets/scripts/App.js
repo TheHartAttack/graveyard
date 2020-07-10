99dad37f7cfbff6629cfd83f017b0f3efbe67cf5
Dan <danhart21390@gmail.com>
2020-07-10 08:34:16 +0100

    Completed reveal on scroll feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../styles/styles.css'
 import Menu from './modules/Menu'
+import Reveal from './modules/RevealOnScroll'
 
 let menu = new Menu()
+new Reveal(document.querySelectorAll('.menu-item'), 75)
+new Reveal(document.querySelectorAll('.review'), 75)
 
 if (module.hot){
     module.hot.accept()
