5daa90d22f024f777a9077e6adc2fbc1d2136a47
Dan <danhart21390@gmail.com>
2020-07-19 06:05:10 +0100

    Added cocktails and renamed bar to Apocalypse

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const postCSSPlugins = [
     require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
+    require('postcss-extend'),
     require('autoprefixer')
 ]
 
